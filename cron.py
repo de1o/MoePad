4db945785348afeadc879dbeea55514f4a6b53fb
deloeating <deloeating@gmail.com>
2013-06-26 15:03:02 +0800

    change cron strategy

--- cron.py
+++ cron.py
@@ -20,10 +20,16 @@ def clean_cached_items():
 def send_reauth_email():
     urllib2.urlopen(reauth_url)
 
-update_sched = Scheduler()
-update_sched.daemonic = False
-update_sched.add_interval_job(check_update, minutes=5)
-update_sched.start()
+# need server timezone should be GMT+8
+day_update_sched = Scheduler()
+day_update_sched.daemonic = False
+day_update_sched.add_cron_job(check_update, hour='7-23', minute='10,30,50', second=0)
+day_update_sched.start()
+
+night_update_sched = Scheduler()
+night_update_sched.daemonic = False
+night_update_sched.add_cron_job(check_update, hour='0-6', minute=10, second=0)
+night_update_sched.start()
 
 clean_cached_sched = Scheduler()
 clean_cached_sched.daemonic = False
