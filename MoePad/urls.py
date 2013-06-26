b0e0951872773f9e70407af5eceb965f43aa8c2b
deloeating <deloeating@gmail.com>
2013-06-26 10:18:58 +0800

    add robots.txt process, reject all search engines

--- MoePad/urls.py
+++ MoePad/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 import CronWeibo
 from django.contrib import admin
 admin.autodiscover()
+from django.http import HttpResponse
 
 
 urlpatterns = patterns('',
@@ -11,10 +12,11 @@ urlpatterns = patterns('',
     url(r'^sinacallback/', 'CronWeibo.views.sinacallback'),
     url(r'^tencentcallback/', 'CronWeibo.views.tencentcallback'),
     url(r'^clean_auth/', 'CronWeibo.views.clean_auth'),
-   	url(r'^send/', 'CronWeibo.views.send'),
-   	url(r'^re_auth_sina/', 'CronWeibo.views.re_auth_sina'),
-   	url(r'^re_auth_tencent/', 'CronWeibo.views.re_auth_tencent'),
-   	url(r'^send_reauth_mail/', 'CronWeibo.views.send_reauth_mail'),
-   	url(r'^clean_cached_items/', 'CronWeibo.views.clean_cached_items'),
-   	url(r'^$', 'CronWeibo.views.index'),
+    url(r'^send/', 'CronWeibo.views.send'),
+    url(r'^re_auth_sina/', 'CronWeibo.views.re_auth_sina'),
+    url(r'^re_auth_tencent/', 'CronWeibo.views.re_auth_tencent'),
+    url(r'^send_reauth_mail/', 'CronWeibo.views.send_reauth_mail'),
+    url(r'^clean_cached_items/', 'CronWeibo.views.clean_cached_items'),
+    url(r'^$', 'CronWeibo.views.index'),
+    url(r'^robots\.txt$', lambda r: HttpResponse("User-agent: *\nDisallow: /", mimetype="text/plain")),
 )
