e5aa199c6c02df31a1069289d99856df24a90c5e
deloeating <deloeating@gmail.com>
2013-09-01 21:45:10 +0800

    add mpconfig table
    use db data instead of config file

--- MoeWiki/admin.py
+++ MoeWiki/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from MoeWiki.models import WikiItems, AlreadlyRetweeted
+from MoeWiki.models import WikiItems, AlreadlyRetweeted, RecentUpdateItems, MoePadConfig
 
 admin.site.register(WikiItems)
 admin.site.register(AlreadlyRetweeted)
+admin.site.register(RecentUpdateItems)
+admin.site.register(MoePadConfig)
